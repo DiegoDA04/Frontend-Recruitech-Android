bb6053035fd2f77e78cbac9be246eec6fba92c2c
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2023-04-30 22:55:28 -0500

    feat: added Typography and text styles for Recruitech Theme

--- app/src/main/java/pe/edu/notcodingdevs/recruitech/ui/theme/Type.kt
+++ app/src/main/java/pe/edu/notcodingdevs/recruitech/ui/theme/Type.kt
@@ -2,18 +2,47 @@ package pe.edu.notcodingdevs.recruitech.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import pe.edu.notcodingdevs.recruitech.R
+
+// Recruitech Typography
+
+val RubikFont = FontFamily(
+    Font(R.font.rubik_light, FontWeight.Light),
+    Font(R.font.rubik_regular, FontWeight.Normal),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_semibold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RubikFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = RubikFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = RubikFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = RubikFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = RubikFont,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
