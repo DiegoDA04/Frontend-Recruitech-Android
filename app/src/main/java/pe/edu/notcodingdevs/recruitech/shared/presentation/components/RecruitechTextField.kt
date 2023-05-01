136b17b8abaf25f4ebdc57d7e7eae419febfcf5d
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2023-05-01 00:22:18 -0500

    feat(shared): added reusable text field component

--- app/src/main/java/pe/edu/notcodingdevs/recruitech/shared/presentation/components/RecruitechTextField.kt
+++ app/src/main/java/pe/edu/notcodingdevs/recruitech/shared/presentation/components/RecruitechTextField.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import pe.edu.notcodingdevs.recruitech.ui.theme.Grey20
 import pe.edu.notcodingdevs.recruitech.ui.theme.Grey40
@@ -34,6 +35,7 @@ fun RecruitechTextField(
             input.value = it
         },
         shape = RoundedCornerShape(8.dp),
+        textStyle = MaterialTheme.typography.labelMedium,
         colors = TextFieldDefaults.outlinedTextFieldColors(
             textColor = Grey40,
             containerColor = Color.White,
