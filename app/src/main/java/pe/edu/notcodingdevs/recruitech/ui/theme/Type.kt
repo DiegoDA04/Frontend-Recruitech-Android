06e5261130bb2cea8be9953db73cfac327ad83db
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2023-05-03 00:27:24 -0500

    feat(theme): Added text styles

--- app/src/main/java/pe/edu/notcodingdevs/recruitech/ui/theme/Type.kt
+++ app/src/main/java/pe/edu/notcodingdevs/recruitech/ui/theme/Type.kt
@@ -5,59 +5,34 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import pe.edu.notcodingdevs.recruitech.R
 
 // Recruitech Typography
-
-val RubikFont = FontFamily(
-    Font(R.font.rubik_light, FontWeight.Light),
-    Font(R.font.rubik_regular, FontWeight.Normal),
-    Font(R.font.rubik_medium, FontWeight.Medium),
-    Font(R.font.rubik_semibold, FontWeight.SemiBold)
+val LatoFont = FontFamily(
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_black, FontWeight.Black)
 )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = RubikFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 20.sp,
+
+    headlineMedium = TextStyle(
+        fontFamily = LatoFont,
+        fontWeight = FontWeight.Black,
+        fontSize = 28.sp,
+        color = Blue80,
+        letterSpacing = 0.25.sp
     ),
-    bodyMedium = TextStyle(
-        fontFamily = RubikFont,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-    ),
-    labelLarge = TextStyle(
-        fontFamily = RubikFont,
-        fontWeight = FontWeight.Medium,
-        fontSize = 18.sp,
-    ),
-    labelMedium = TextStyle(
-        fontFamily = RubikFont,
-        fontWeight = FontWeight.Medium,
-        fontSize = 16.sp,
-    ),
-    labelSmall = TextStyle(
-        fontFamily = RubikFont,
-        fontWeight = FontWeight.Medium,
-        fontSize = 14.sp,
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
+    headlineSmall = TextStyle(
+        fontFamily = LatoFont,
+        fontWeight = FontWeight.Black,
+        fontSize = 24.sp,
+        color = Blue80,
         letterSpacing = 0.5.sp
     )
-    */
 )
