4ef1014ad82e9d73ede9334c707b6cfb17cb92bb
LenninHuaman <u20201b975@upc.edu.pe>
2023-05-04 01:11:35 -0500

    Added feature Login with all necessary files

--- app/src/main/java/pe/edu/notcodingdevs/recruitech/MainActivity.kt
+++ app/src/main/java/pe/edu/notcodingdevs/recruitech/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.notcodingdevs.recruitech.feature_login.presentation.login.Login
+import pe.edu.notcodingdevs.recruitech.feature_login.presentation.login.LoginViewModel
 import pe.edu.notcodingdevs.recruitech.ui.theme.RecruitechTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Login(LoginViewModel(), this)
                 }
             }
         }
